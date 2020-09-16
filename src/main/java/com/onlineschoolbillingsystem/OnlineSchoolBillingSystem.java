c595f4c6206c0152ab4198f30e62f18e0fbd0295
Dilum De Silva <dilumdesilva.info@gmail.com>
2020-09-16 17:53:56 +0530

    Add: initial spring project This commit contains initial project with application.properties and dependencies.

---
+++ src/main/java/com/onlineschoolbillingsystem/OnlineSchoolBillingSystem.java
@@ -0,0 +1,13 @@
+package com.onlineschoolbillingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineSchoolBillingSystem {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineSchoolBillingSystem.class, args);
+    }
+
+}
